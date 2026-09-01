da2bbe281fbf948e7584da7de753092a539e2992
Harsha Vardhan Reddy <harshavardhanreddy9491@gmail.com>
2026-09-01 22:41:50 +0530

    Update Update.java

--- backend/src/main/java/jar/controller/Update.java
+++ backend/src/main/java/jar/controller/Update.java
@@ -9,42 +9,18 @@ import org.springframework.web.bind.annotation.*;
 
 import jar.model.Student;
 import jar.repo.StudentRepo;
+import jar.service.Dservice;
 
 @RestController
 @RequestMapping("/api/v1")
 public class Update {
 
     @Autowired
-    StudentRepo db;
+    Dservice db;
 
     @PutMapping("/{id}")
     public Map<Object, Object> update(@PathVariable Integer id, @RequestBody Student d) {
 
-        Map<Object, Object> res = new HashMap<>();
-
-        Optional<Student> data = db.findById(id);
-
-        if (data.isPresent()) {
-
-            Student s = data.get();
-
-            s.setName(d.getName());
-            s.setEmail(d.getEmail());
-            s.setIp(d.getIp());
-
-            db.save(s);
-
-            res.put("msg", "Student Updated Successfully");
-            res.put("status", 200);
-            res.put("data", s);
-
-        } else {
-
-            res.put("msg", "Student Not Found");
-            res.put("status", 404);
-
-        }
-
-        return res;
+       return db.update(id, d);
     }
 }
