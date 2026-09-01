5f220b550ed242bd055ac45d3380e453f7457ef6
Harsha Vardhan Reddy <harshavardhanreddy9491@gmail.com>
2026-09-01 22:41:25 +0530

    Update Read.java

--- backend/src/main/java/jar/controller/Read.java
+++ backend/src/main/java/jar/controller/Read.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import jar.service.Dservice;
 import jar.repo.StudentRepo;
 
 @RestController
@@ -15,17 +15,12 @@ import jar.repo.StudentRepo;
 public class Read {
 
     @Autowired
-    StudentRepo db;
+    Dservice db;
 
     @GetMapping 
     public Map<Object, Object> read() {
 
-        Map<Object, Object> res = new HashMap<>();
-        res.put("msg", "Student Fetched Successfully");
-        res.put("status", 200);
-        res.put("data", db.findAll());
-
-        return res;
+        return db.read();
     }
     
 }
